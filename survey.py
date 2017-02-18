37072cd1f8e6f2df2cf03eb94a474b9aaf53e11e
Bradford Lynch <blynch41@gmail.com>
2017-02-18 17:50:37 -0500

    Updated with ability to dump data

--- survey.py
+++ survey.py
@@ -10,10 +10,12 @@ import urllib
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
+from google.appengine.api import app_identity
 
 import jinja2
 import webapp2
 import numpy as np
+import pickle
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -34,21 +36,26 @@ class Participant(ndb.Model):
     mouse_x_pos = ndb.TextProperty()
     mouse_y_pos = ndb.TextProperty()
     result = ndb.StringProperty(indexed=False)
+    follow_up = ndb.StringProperty(indexed=False)
+    profile = ndb.StringProperty(indexed=False)
 
 # Globals
 ad_categories = ['Air Travel', 'Food and Drinks', 'TV Shows', 'None of the above']
 products = ['Eyewear', 'Headphones', "Women's Fashion", "Men's Fashion", 'Soda', 'None of these products']
 ads = {
     'Air Travel':'delta',
-    'Food and Drinks':'delta',
-    'TV Shows':'delta'
+    'Food and Drinks':'pepsi',
+    'TV Shows':'tbbt'
 }
+
+scale = {1:'1 - Not at all', 2:'2', 3:'3 - Somewhat', 4:'4', 5:'5 - Very'}
+
 t_show = 100
 t_hide = 2500
 
 # Helper functions
 def pickAd(interests):
-    if len(interests) == 0:
+    if (len(interests) == 0) or (ad_categories[-1] in interests):
         cats = [cat for cat in ad_categories if 'None' not in cat]
         ad_cat = cats[np.random.randint(0,len(cats))]
     else:
@@ -72,13 +79,18 @@ def pickAd(interests):
 
 # [START main_page]
 class MainPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
+
+class IntroQues(webapp2.RequestHandler):
 
     def get(self):
         template_values = {
             'ad_cats':ad_categories,
             'products':products
         }
-        template = JINJA_ENVIRONMENT.get_template('index.html')
+        template = JINJA_ENVIRONMENT.get_template('intro_questions.html')
         self.response.write(template.render(template_values))
 
     def post(self):
@@ -169,10 +181,55 @@ class Click(webapp2.RequestHandler):
 
         template_values = {
             'user_id':user_id,
-            'sub':(browser, click, mouse_x_pos)
+            'scale':scale
         }
 
-        template = JINJA_ENVIRONMENT.get_template('submission.html')
+        template = JINJA_ENVIRONMENT.get_template('follow_up_questions.html')
+        self.response.write(template.render(template_values))
+
+class FollowUpQues(webapp2.RequestHandler):
+
+    def post(self):
+        user_id = self.request.get('user_id')
+        follow_up_res = self.request.get('responses')
+
+        # Get user data object from ndb
+        user_key = ndb.Key(urlsafe=user_id)
+        user = user_key.get()
+
+        # Save ad survey results
+        user.follow_up = follow_up_res
+        user.put()
+
+        template_values = {
+            'user_id':user_id,
+            'scale':scale
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('final_questions.html')
+        self.response.write(template.render(template_values))
+
+
+class FinalQues(webapp2.RequestHandler):
+
+    def post(self):
+        user_id = self.request.get('user_id')
+        profile_res = self.request.get('responses')
+
+        # Get user data object from ndb
+        user_key = ndb.Key(urlsafe=user_id)
+        user = user_key.get()
+
+        # Save ad survey results
+        user.profile = profile_res
+        user.put()
+
+        template_values = {
+            'user_id':user_id,
+            'code':user_id + '153624'
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('complete.html')
         self.response.write(template.render(template_values))
 
 class AdTech(webapp2.RequestHandler):
@@ -180,6 +237,13 @@ class AdTech(webapp2.RequestHandler):
     def get(self):
         template = JINJA_ENVIRONMENT.get_template('intro_to_tech.html')
         self.response.write(template.render())
+
+class DumpData(webapp2.RequestHandler):
+
+    def get(self):
+        qry = Participant.query().fetch()
+        pickledData = pickle.dumps(qry)
+        self.response.write(pickledData)
 
 class Delta(webapp2.RequestHandler):
 
@@ -221,11 +285,15 @@ class TBBT(webapp2.RequestHandler):
 # [START app]
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/intro', IntroQues),
     ('/submit', ShowAd),
     ('/intro_to_tech', AdTech),
     ('/click', Click),
     ('/delta', Delta),
     ('/pepsi', Pepsi),
     ('/tbbt', TBBT),
+    ('/follow_up', FollowUpQues),
+    ('/final', FinalQues),
+    ('/dumpData', DumpData),
 ], debug=True)
 # [END app]
